f166181ccd64c23a76a466a1432361cc921c35f5
Ola Ishola <olaishola@hotmail.co.uk>
2020-05-27 00:04:09 +0100

    working on the FE

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ function getRecipe() {
         .then((res) => res)
         .then((res) => {
             let recipe = res.data.meals[0];
-            console.log(recipe);
-            showOutput(res.data.meals[0]);
+            console.log(res);
+            showOutput(recipe);
         })
         .catch((err) => {
             console.error(err);
@@ -15,21 +15,56 @@ function getRecipe() {
 }
 
 function showOutput(recipe) {
-    // console.log(recipe);
+    let ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        if (recipe[`strIngredient${i}`]) {
+            ingredients.push(`${recipe[`strIngredient${i}`]}`);
+            console.log(ingredients);
+        } else {
+            break;
+        }
+    }
     document.getElementById("result").innerHTML = `
     <div class = "container">
-    <div class="card card-body mb-4 mt-3">
-    <h3>Recipe: ${recipe.strMeal}</h3>
-    <pre>Country: ${recipe.strArea}</pre>
+    <div class=" mb-4 mt-3">
+    <h3 class="text-center">Recipe: ${recipe.strMeal}</h3>
+    <ul class = "center">
+    <li>Country: ${recipe.strArea}</li>
+    <li>Tag: ${recipe.strTags}</li>
+    <li>Category: ${recipe.strCategory}</li>
+    </ul>
     </div>
-    <div class = "card" id = "img-container">
-    <img src = ${recipe.strMealThumb} id = "dish" alt = "image of a recipe>
-    
-    <div class="mt-3 text-center" id="instruction">
+
+    <div id = "img-container">
+
+    <div id = "dish">
+    <img src = ${recipe.strMealThumb} alt = "image of a recipe>
+    </div>
+
+    <div class="instruction">
+    <h2 class = "text-center">Instruction</h2>
+    <p id="text">
+    ${recipe.strInstructions}</p>
+    </div>
+
+    </div>
+    <hr>
+
+    <div class = "media">
+    <div class="">
+    <h5>Ingredients:</h5>
+    <ul>
+    ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+    </ul>
+    </div>
    
-    ${recipe.strInstructions}
+    <div class="video">
+    <h5>Recipe Video</h5>
+    <iframe src = "https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}" width = "500" height = "420"></iframe>
     </div>
+
     </div>
+    
     
     
     </div>
